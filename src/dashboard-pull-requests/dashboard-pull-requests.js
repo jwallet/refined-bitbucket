adf87e102a35cd344422e6186befa53115d2c867
jwallet <joseouellet@gmail.com>
2020-05-23 20:58:48 -0400

    fixes build

--- src/dashboard-pull-requests/dashboard-pull-requests.js
+++ src/dashboard-pull-requests/dashboard-pull-requests.js
@@ -12,7 +12,7 @@ export const filterStyle = {
 
 const classHide = '__refined_bitbucket_hide'
 
-export const classFilter = id => `__refined_bitbucket_filter_${id}`
+export const classFilter = (id: string) => `__refined_bitbucket_filter_${id}`
 
 export const filterNames = {
     successfulBuilds: 'successfulBuilds',
@@ -76,12 +76,12 @@ async function newCheckbox(filterName, notMe) {
     return <input name={filterName} style={filterStyle} type="checkbox" />
 }
 
-function getElementUuid(el, attr) {
-    const user = JSON.parse(el.getAttribute(attr))
+function getElementUuid(el: HTMLElement, attr: string) {
+    const user = JSON.parse(el.getAttribute(attr) || '{}')
     return user ? user.uuid : ''
 }
 
-function performHide(el, filterName) {
+function performHide(el: HTMLElement, filterName: string) {
     const row = el.closest('tr')
     if (!row) return
 
@@ -90,32 +90,35 @@ function performHide(el, filterName) {
     return row
 }
 
-function hide(querySelector, filterName, notMe = false) {
+function hide(querySelector: string, filterName: string, notMe = false) {
+    const container: HTMLElement = (document.getElementById(
+        'pullrequests'
+    ): any)
+
     if (notMe) {
-        const me = getElementUuid(document.body, 'data-current-user')
-        document
-            .getElementById('pullrequests')
-            .querySelectorAll(filtersHidingSelector.notMe)
-            .forEach(el => {
-                const uuidFound = getElementUuid(el, 'data-user')
-                if (uuidFound !== me) return
-                performHide(el, filterName)
-            })
+        const me: string = getElementUuid(
+            (document.body: any),
+            'data-current-user'
+        )
+        container.querySelectorAll(filtersHidingSelector.notMe).forEach(el => {
+            const uuidFound = getElementUuid(el, 'data-user')
+            if (uuidFound !== me) return
+            performHide(el, filterName)
+        })
     }
 
-    const els = document
-        .getElementById('pullrequests')
-        .querySelectorAll(querySelector)
+    const els = container.querySelectorAll(querySelector)
 
     if (!els || els.length === 0) return
 
     els.forEach(el => performHide(el, filterName))
 }
 
-function show(filterName) {
-    const els = document
-        .getElementById('pullrequests')
-        .querySelectorAll(`tr.${classFilter(filterName)}`)
+function show(filterName: string) {
+    const container: HTMLElement = (document.getElementById(
+        'pullrequests'
+    ): any)
+    const els = container.querySelectorAll(`tr.${classFilter(filterName)}`)
     els.forEach(el => {
         el.classList.remove(classFilter(filterName))
         el.classList.remove(classHide)
@@ -123,13 +126,14 @@ function show(filterName) {
     })
 }
 
-function save(filterName, checked) {
-    localStorage.setItem([classFilter(filterName)], checked)
+function save(filterName: string, checked: boolean) {
+    localStorage.setItem(classFilter(filterName), checked.toString())
 }
 
 function onFilterSuccessfulBuilds(e) {
-    save(filterNames.successfulBuilds, e.target.checked)
-    return e.target.checked
+    const { checked } = (e.target: any)
+    save(filterNames.successfulBuilds, checked)
+    return checked
         ? hide(
               filtersHidingSelector.successfulBuilds,
               filterNames.successfulBuilds
@@ -138,8 +142,9 @@ function onFilterSuccessfulBuilds(e) {
 }
 
 function onFilterAllTasksResolved(e) {
-    save(filterNames.allTasksResolved, e.target.checked)
-    return e.target.checked
+    const { checked } = (e.target: any)
+    save(filterNames.allTasksResolved, checked)
+    return checked
         ? hide(
               filtersHidingSelector.allTasksResolved,
               filterNames.allTasksResolved
@@ -148,8 +153,9 @@ function onFilterAllTasksResolved(e) {
 }
 
 function onFilterNeedsMyApproval(e) {
-    save(filterNames.needsMyApproval, e.target.checked)
-    return e.target.checked
+    const { checked } = (e.target: any)
+    save(filterNames.needsMyApproval, checked)
+    return checked
         ? hide(
               filtersHidingSelector.needsMyApproval,
               filterNames.needsMyApproval,
